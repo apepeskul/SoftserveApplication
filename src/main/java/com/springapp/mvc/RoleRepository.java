289b4703db0f00ae08d5aa4109c37965f93e336d
iksuy <denis.gavrus@gmail.com>
2013-11-23 19:14:50 +0300

    Packages was restructured

--- src/main/java/com/springapp/mvc/RoleRepository.java
+++
@@ -1,5 +0,0 @@
-package com.springapp.mvc;
- import org.springframework.data.jpa.repository.JpaRepository;
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findById (Long id);
-}
